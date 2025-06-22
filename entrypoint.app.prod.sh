77b4e0f4ec776203d92296fac7c6370a3d2ef189
Shogo Fukushima <59794072+fksms@users.noreply.github.com>
2025-06-23 02:11:18 +0900

    refactor:

---
+++ entrypoint.app.prod.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# WireGuard Setup
+ip link add dev ${WG_INTERFACE_NAME} type wireguard
+ip address add dev ${WG_INTERFACE_NAME} ${WG_INTERFACE_IP}
+
+# Start the application
+exec dumb-init node server.js
---
+++ entrypoint.rploxy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Nginx Start
+nginx -g 'daemon off;'
