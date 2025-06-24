22f3bc7067bda4e50a33e8fd77a7aa58ad918aa8
Shogo Fukushima <59794072+fksms@users.noreply.github.com>
2025-06-24 15:22:35 +0000

    feat: Added a traffic blocking rule.

--- entrypoint.dev.sh
+++ entrypoint.dev.sh
@@ -3,6 +3,12 @@
 # Enable IP forwarding
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
+# Add client-to-client blocking rules
+iptables -I FORWARD -i ${WG_INTERFACE_NAME} -o ${WG_INTERFACE_NAME} -j DROP
+
+# Drop ICMP packets
+iptables -A INPUT -p icmp -j DROP
+
 # Create and activate the Wireguard interface
 ip link add dev ${WG_INTERFACE_NAME} type wireguard
 ip address add dev ${WG_INTERFACE_NAME} ${WG_INTERFACE_IP}
